43d839c55e0cec4b1c21d83a24b1b4317506e5fa
pkubysheva <71191097+pkubysheva@users.noreply.github.com>
2020-10-06 21:10:19 +0300

    Add files via upload

---
+++ 222.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+	setlocale(LC_ALL, "ru");
+	int a, b, c, d;
+
+	cout Ђ "¬ведите 4 числа" Ђ endl;
+	cin ї a ї b ї c ї d;
+
+
+	cout Ђ "—реднее арифметическое четырех чисел =" Ђ(double)(a + b + c + d) / 4 Ђ endl;
+}
